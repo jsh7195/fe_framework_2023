8dc094b86e35ab1a3db47a7d5ae9768515571f72
shji <jsh7195gg@gmail.com>
2023-03-10 17:07:57 +0900

    axios 보완

--- src/lib/network.ts
+++ src/lib/network.ts
@@ -1,39 +1,73 @@
-import axios, { AxiosError } from 'axios';
-import qs from 'qs';
+import axios, {
+  AxiosError,
+  AxiosResponse,
+  InternalAxiosRequestConfig,
+} from 'axios';
+import _ from 'lodash';
+//   import {getData} from './storage';
+import { handleErrorResponse } from './util';
 
-const axiosHeaders = {
-    'Content-Type': 'application/json;charset=utf-8;',
-};
+interface IReponseData {
+  data?: any;
+  errorCode?: string;
+}
 
-const _axios = axios.create({
-    baseURL: 'http://localhost:3000',
-    // baseURL: 'https://bpdiarydev.openit.co.kr',
-    headers: axiosHeaders,
-    timeout: 100000,
-    paramsSerializer: {
-        encode: (params) => qs.stringify(params, { arrayFormat: 'repeat' })
-        //indexes: null // by default: false
-    }
+const instance = axios.create({
+  baseURL: '',
 });
 
-
-_axios.interceptors.response.use(
-    (response) => {
-        return response;
-    },
-    async (error: AxiosError) => {
-
+// 요청 인터셉터 설정
+axios.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    //   const token = getData('token');
+    const token = 'token here'
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
     }
+
+    return config;
+  },
+  (error: AxiosError) => {
+    // 요청 에러 처리
+    return Promise.reject(error);
+  },
 );
 
-_axios.interceptors.request.use(
-    (request) => {
+// 응답 인터셉터
+instance.interceptors.response.use(
+  (response: AxiosResponse) => {
+    return response;
+  },
+  (error: AxiosError) => {
+    // HTTP 응답이 200이 아닌 경우
+    if (error.response && _.isEmpty(error.response)) {
+      const status = error.response?.status;
+      const data = error.response?.data as IReponseData;
 
-        return request;
-    },
-    (error) => {
+      let errorCode = '500';
 
+      if (data && _.has(data, 'errorCode') && data.errorCode) {
+        errorCode = data.errorCode;
+      }
+
+      if (status >= 400 && status < 500) {
+        // 400 ~ 499 에러 처리
+        handleErrorResponse(status, errorCode);
+      } else if (status >= 500) {
+        // 500 에러 처리
+        handleErrorResponse(status, '500');
+      } else if (data && errorCode) {
+        // 서버에서 발생한 에러 처리
+        throw new Error(errorCode);
+      }
+    } else if (error.request) {
+      // 요청이 전송되지 않은 경우
+      console.error(error.request);
+    } else {
+      console.error(error.message);
     }
+    return Promise.reject(error);
+  },
 );
 
-export default _axios;
+export default instance;
